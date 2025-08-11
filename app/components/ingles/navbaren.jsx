309e0e0f8c59e9460dea7897fd6eb38bea6d5758
byQuesters <albertpoambez@gmail.com>
2025-08-11 02:09:55 -0600

    navbar updated

--- app/components/ingles/navbaren.jsx
+++ app/components/ingles/navbaren.jsx
@@ -1,30 +1,67 @@
-// navbar.jsx
-
 'use client';
-
 /* IMPORT PRINCIPAL LIBRARYS */
 import Link from 'next/link';
-
+import { useState, useEffect } from 'react';
 /* IMPORT CSS */
 import "../../css/navbar.css"
-
 /* BOOTSTRAP ICONS */
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 export default function NavbarEN() {
-    return (
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isTransitioning, setIsTransitioning] = useState(false);
 
-        <div className="navbar">
-            <div className="logo">
-                <Link className="link" href="/">Al</Link>
-            </div>
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const shouldBeScrolled = scrollY > 100;
+      
+      if (shouldBeScrolled !== isScrolled && !isTransitioning) {
+        setIsTransitioning(true);
+        
+        // Iniciar fade out
+        setTimeout(() => {
+          setIsScrolled(shouldBeScrolled);
+          
+          // Completar fade in
+          setTimeout(() => {
+            setIsTransitioning(false);
+          }, 300); // Duración del fade in
+        }, 300); // Duración del fade out
+      }
+    };
 
-            <div className="access">
-                <a className="link" href="#"><i className="bi bi-house-fill"></i><span>Home</span></a>
-                <a className="link" href="./PersonalEN"><i className="bi bi-person-raised-hand"></i><span>Solo</span></a>
-                <a className="link" href="./GrupalEN"><i className="bi bi-people-fill"></i><span>In Group</span></a>
-             </div>
-            
-        </div>
-    );
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [isScrolled, isTransitioning]);
+
+  // Determinar las clases CSS
+  const getNavbarClasses = () => {
+    let classes = 'navbar';
+    
+    if (isScrolled) {
+      classes += ' navbar-header';
+    }
+    
+    if (isTransitioning) {
+      classes += ' navbar-fade-out';
+    } else {
+      classes += ' navbar-fade-in';
+    }
+    
+    return classes;
+  };
+
+  return (
+    <div className={getNavbarClasses()}>
+      <div className="logo">
+        <Link className="link" href="/">Al</Link>
+      </div>
+      <div className="access">
+        <a className="link" href="#"><i className="bi bi-house-fill"></i><span>Home</span></a>
+        <a className="link" href="./PersonalEN"><i className="bi bi-person-raised-hand"></i><span>Solo</span></a>
+        <a className="link" href="./GrupalEN"><i className="bi bi-people-fill"></i><span>In Group</span></a>
+      </div>
+    </div>
+  );
 }
